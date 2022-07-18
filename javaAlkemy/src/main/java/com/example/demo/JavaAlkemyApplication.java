0c65894d9a34d67843d222ff9fcd862bdc0cb2b8
fabri_z <fabricoltro@hotmail.com>
2022-07-18 01:01:31 -0300

    first commit

---
+++ javaAlkemy/src/main/java/com/example/demo/JavaAlkemyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAlkemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAlkemyApplication.class, args);
+	}
+
+}
